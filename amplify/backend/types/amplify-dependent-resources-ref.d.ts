a6b20f98f21cfaad00b3450d3b79b5c00f19300b
Ryohei Matsuba <r.matsuba@genbasupport.com>
2022-10-28 13:27:46 +0900

    認証機能追加
    - amplify add auth
    - amplify push --y

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "matsubaamplify68020bcd": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
